97d1d4739c5ff85920ffb9031af5620ff46da6a4
Fatima Shafiq <fatimarajpoott@gmail.com>
2023-04-16 22:35:29 +0500

    2nd-js

--- script.js
+++ script.js
@@ -26,3 +26,28 @@ async function checkWeather(city){
     weather_body.style.display = "flex";
     temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
     description.innerHTML = `${weather_data.weather[0].description}`;
+    
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+            weather_img.src = "clouds.png";
+            break;
+        case 'Clear':
+            weather_img.src = "clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "mist.png";
+            break;
+            case 'drizzle':
+            weather_img.src = "drizzle.png";
+            break;
+        case 'Snow':
+            weather_img.src = "snow.png";
+            break;
+    }
+    console.log(weather_data);
+}
